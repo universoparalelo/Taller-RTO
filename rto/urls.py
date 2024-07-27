2a802df68f69ef9a36c9d39224eac86721df168a
UniversoParalelo <cmartinrodich@gmail.com>
2024-07-27 19:42:19 -0300

    change folder static -> media and create a static empty

--- rto/urls.py
+++ rto/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tarifas import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +24,4 @@ urlpatterns = [
     path('consultar/', views.consultarTarifa),
     path('ver/', views.verTarifa),
     path('verTodasTarifas/', views.verTodasTarifas)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
