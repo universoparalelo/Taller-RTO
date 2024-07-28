a5a34b58a6b6c69d4699e15827a73b1c07b1954a
UniversoParalelo <cmartinrodich@gmail.com>
2024-07-28 17:26:46 -0300

    change location of static

--- rto/urls.py
+++ rto/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('verTodasTarifas/', views.verTodasTarifas)
 ] 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
