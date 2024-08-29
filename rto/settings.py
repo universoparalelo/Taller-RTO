e67f42452f0e46976dfa45cf52c93df15cad3591
UniversoParalelo <cmartinrodich@gmail.com>
2024-08-29 09:19:44 -0300

    Fixing bugs

--- rto/settings.py
+++ rto/settings.py
@@ -87,6 +87,12 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR/'db.sqlite3',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
